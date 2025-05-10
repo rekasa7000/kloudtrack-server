d682d1533c914a85c53b94e8efadf158df060387
rekasa <81959401+rekasa7000@users.noreply.github.com>
2025-05-10 19:23:22 +0800

    Delete dist directory

--- dist/modules/user/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("./user.controller");
-const router = (0, express_1.Router)();
-router.post("/users", user_controller_1.createUser);
-router.post("/users/bulk", user_controller_1.bulkCreateUsers);
-exports.default = router;
